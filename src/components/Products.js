324b368b4c74ce7abbde1245577e3cc2ffccb6cd
Bruno <brunohgrau99@gmail.com>
2024-04-03 01:04:28 -0300

    HomeScreen

---
+++ src/components/Products.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Rating from "@mui/material/Rating";
+import Link from "@mui/material/Link";
+import products from "../mocks/products";
+
+const Products = () => {
+  const [value, setValue] = useState(2);
+
+  return (
+    <Container
+      fixed
+      id="products"
+      sx={{
+        py: { xs: 16 },
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: { xs: "center", sm: "start" },
+        }}
+      >
+        <Typography component="h2" variant="h4" color="text.primary">
+          Our Products
+        </Typography>
+      </Box>
+      <Grid container spacing={1}>
+        {products.map((product, index) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            key={product.id}
+            sx={{
+              display: "flex",
+              gap: { xs: 3, sm: 6 },
+              alignItems: "center",
+              justifyContent: { xs: "center", sm: "start" },
+            }}
+          >
+            <Card sx={{ width: 250, mt: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="../../images/airpods.jpg"
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                  <Link href="/details" color="text.primary">
+                    Product Name
+                  </Link>
+                </Typography>
+
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                      setValue(newValue);
+                    }}
+                  />
+                  <Typography
+                    variant="caption"
+                    color="text.secondary"
+                    sx={{ ml: 2 }}
+                  >
+                    1 Review
+                  </Typography>
+                </Box>
+                <Typography variant="h4" color="text.secondary" sx={{ mt: 2 }}>
+                  $599
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Products;
--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import store from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomeScreen />} />
+    </Route>
+  )
 );
 
 async function deferRender() {

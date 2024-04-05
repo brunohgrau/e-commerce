65e20811f36f34e606a26bf538b23c60aba98d69
Bruno <brunohgrau99@gmail.com>
2024-04-05 03:43:56 -0300

    cart

---
+++ src/slices/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.id === item.id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x.id === existItem.id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      // Calculate shipping price
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // Calculate rax price
+      // Calculate total price
+    },
+  },
+});
+
+export default cartSlice.reducer;
